5ab6a16756f309b5a0dbf3fbabcc1ea0269e6e58
The-Earth <tigerzeng97@gmail.com>
2019-03-17 15:44:00 +0800

    [Modify]: Recalculated angular coefficients. [Add]: Readme

---
+++ A3/README.md
@@ -0,0 +1,3 @@
+None sense here. Looking into them is only a waste of time.
+
+Calculating inbound and outbound energy flux of six faces in a box. Energy fluxes are only inside the box and the box has a grey body correction factor of 0.9.
--- A3/angpara.py
+++ A3/angpara.py
@@ -3,22 +3,22 @@ from numpy import linalg
 
 # Size: 2*2*3.84
 # Angular parameter
-par1 = 1.20866658758691e-1
-par2 = 6.85895888785527e-2
-par3 = 2.81382200569582e-1
-par4 = 1.2062604242338e-1
-par5 = 2.3868285729087e-1
+par1 = 7.3591994181087100E-02  # Parallel bottom -> top
+par2 = 2.3160200145472900E-01  # Orthogonal bottom -> side
+par3 = 2.3868285729087200E-01  # Orthogonal side -> side
+par4 = 2.8138220056958200E-01  # Parallel side -> side
+par5 = 1.2062604242433800E-01  # Orthodonal side -> bottom
 # Absorption rate
 a = 0.9
 
-coefficient_mat = np.mat([[1 / (a - 1), par2, par2, par2, par2, par1],
-                          [par4, 1 / (a - 1), par5, par3, par5, par4],
-                          [par4, par5, 1 / (a - 1), par5, par3, par4],
-                          [par4, par3, par5, 1 / (a - 1), par5, par4],
-                          [par4, par5, par3, par5, 1 / (a - 1), par4],
-                          [par1, par2, par2, par2, par2, 1 / (a - 1)]]).transpose()
+coefficient_mat = np.mat([[1 / (a - 1), par5, par5, par5, par5, par1],
+                          [par2, 1 / (a - 1), par3, par4, par3, par2],
+                          [par2, par3, 1 / (a - 1), par3, par4, par2],
+                          [par2, par4, par3, 1 / (a - 1), par3, par2],
+                          [par2, par3, par4, par3, 1 / (a - 1), par2],
+                          [par1, par5, par5, par5, par5, 1 / (a - 1)]], dtype="float64")
 
 constant_vec = np.array([1 / (a - 1), 0, 0, 0, 0, 0])
-result = linalg.solve(coefficient_mat, constant_vec)
-
-print(sum(result))
+result = linalg.solve(coefficient_mat, constant_vec)    # Outbound flux
+s = result[1:] / (1-a)
+print(f'Outbound: {result}\nInbound: {s}')
