eab82195ebf49443da2c38faf16f619befde928f
The-Earth <tigerzeng97@gmail.com>
2019-03-19 11:29:46 +0800

    [Modify]: Reorganized output

--- A3/angpara.py
+++ A3/angpara.py
@@ -7,7 +7,7 @@ par1 = 7.3591994181087100E-02  # Parallel bottom -> top
 par2 = 2.3160200145472900E-01  # Orthogonal bottom -> side
 par3 = 2.3868285729087200E-01  # Orthogonal side -> side
 par4 = 2.8138220056958200E-01  # Parallel side -> side
-par5 = 1.2062604242433800E-01  # Orthodonal side -> bottom
+par5 = 1.2062604242433800E-01  # Orthogonal side -> bottom
 # Absorption rate
 a = 0.9
 
@@ -19,6 +19,7 @@ coefficient_mat = np.mat([[1 / (a - 1), par5, par5, par5, par5, par1],
                           [par1, par5, par5, par5, par5, 1 / (a - 1)]], dtype="float64")
 
 constant_vec = np.array([1 / (a - 1), 0, 0, 0, 0, 0])
-result = linalg.solve(coefficient_mat, constant_vec)  # Outbound flux
-s = result[1:] / (1 - a)
-print(f'Outbound: {result}\nInbound: {s}')
+outbound = linalg.solve(coefficient_mat, constant_vec)  # Outbound flux
+inbound = list(outbound[1:] / (1 - a))
+inbound.insert(0, (outbound[0] - 1) / (1 - a))
+print(f'Outbound: {outbound}\nInbound: {inbound}\nSum of Outbound / Sum of Inbound = {sum(inbound) / sum(outbound)}')
