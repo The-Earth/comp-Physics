28de33b06550eaae4b0b082a7a22f0c7f912dc60
The-Earth <tigerzeng97@gmail.com>
2019-06-16 11:04:50 +0800

    [Add] (par.py) Add average magnetic torque and energy

--- A14/par.py
+++ A14/par.py
@@ -18,13 +18,33 @@ def main():
 
     cv_list = []
     t_list = []
+    mag_list = []
+    ene_list = []
     for res in res_list:
         cv_list.append(res.get()[1])
         t_list.append(res.get()[0])
+        mag_list.append(res.get()[2])
+        ene_list.append(res.get()[3])
 
     plt.cla()
     plt.scatter(t_list, cv_list, c='b')
+    plt.xlabel('温度')
+    plt.ylabel('热容')
     plt.savefig(f'cv-{in_list[0]}-{in_list[-1]}.png')
+    plt.show()
+
+    plt.cla()
+    plt.scatter(t_list, mag_list)
+    plt.xlabel('温度')
+    plt.ylabel('平均磁化强度')
+    plt.savefig(f'mag-{in_list[0]}-{in_list[-1]}.png')
+    plt.show()
+
+    plt.cla()
+    plt.scatter(t_list, ene_list)
+    plt.xlabel('温度')
+    plt.ylabel('平均能量')
+    plt.savefig(f'ene-{in_list[0]}-{in_list[-1]}.png')
     plt.show()
 
 
